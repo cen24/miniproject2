b1c529c6ac5ce5ad2a53ee749e455d56f5f50693
Chinedu Nnaji <cen24@njit.edu>
2019-10-25 20:51:39 -0400

    Task: Seting up Calc. from Calculator Repo

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -1,40 +1,36 @@
 import unittest
-from Calculator import Calculator
+from calculator import calculator
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.calculator = Calculator()
+        self.calculator = calculator()
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.calculator, Calculator)
+        self.assertIsInstance(self.calculator, calculator)
 
     def test_addition(self):
-        self.assertEqual(self.calculator.add(3, 3), 6)
+        self.calculator.add(3, 3)
         self.assertEqual(self.calculator.result, 6)
 
     def test_subtraction(self):
-        self.assertEqual(self.calculator.subtract(3, 3), 0)
+        self.calculator.sub(3, 3)
         self.assertEqual(self.calculator.result, 0)
 
     def test_multiplication(self):
-        self.assertEqual(self.calculator.multiplication(3, 3), 9)
+        self.calculator.times(3, 3)
         self.assertEqual(self.calculator.result, 9)
 
     def test_division(self):
-        self.assertEqual(self.calculator.division(3, 3), 1)
-        self.assertEqual(self.calculator.result, 1)
-
-    def test_division(self):
-        self.assertEqual(self.calculator.division(3, 3), 1)
+        self.calculator.div(3, 3)
         self.assertEqual(self.calculator.result, 1)
 
     def test_square(self):
-        self.assertEqual(self.calculator.square(9), 81)
+        self.calculator.square(9)
         self.assertEqual(self.calculator.result, 81)
 
-    def test_squareroot(self):
-        self.assertEqual(self.calculator.squareroot(81), 9)
+    def test_squareRoot(self):
+        self.calculator.sqrt(81)
         self.assertEqual(self.calculator.result, 9)
 
     def test_results_property(self):
