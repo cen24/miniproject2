b91025a9a1979f5d29d9a5afec108a08fcfd3bc6
rutvik2611 <rutvik2611@gmail.com>
2019-11-01 04:02:41 -0300

    Feature:Added Stat(Mean,median and mode)

---
+++ src/CALCULATOR/Stat.py
@@ -0,0 +1,39 @@
+import statistics
+
+def mean_(list):
+    c = statistics.mean(list)
+    return c
+
+def median_(list):
+    c = statistics.median(list)
+    return c
+
+def mode_(list):
+    c = statistics.mode(list)
+    return c
+
+def stdev_(list):
+    c = statistics.stdev(list)
+    return c
+
+class xyz:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def mean(self, list):
+        self.result = mean_(list)
+        return self.result
+
+    def median_(self, list):
+        self.result = median_(list)
+        return self.result
+
+    def mode_(self, list):
+        self.result = mode_(list)
+        return self.result
+
+    def stdev_(self, list):
+        self.result = stdev_(list)
+        return self.result
