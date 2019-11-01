64f3df59fd55b24f7c42a0e4201ccd6862bd167a
rutvik2611 <rutvik2611@gmail.com>
2019-11-01 03:27:37 -0300

    Feature:Added Read Array function

--- src/Cal2Test.py
+++ src/Cal2Test.py
@@ -1,6 +1,6 @@
 import unittest
 from CALCULATOR.Cal2 import cal2
-from CALCULATOR.readcsv import read_csv
+from CALCULATOR.readcsv import read
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
@@ -12,11 +12,59 @@ class MyTestCase(unittest.TestCase):
 
     def test_add_from_csv(self):
 
-        y = read_csv("uadd.csv")
+        y = read.read_csv("../src/CSV FILES/uadd.csv")
 
         for var in range(len(y)):
             self.cal2.addition(y[0][var], y[1][var])
             self.assertEqual(self.cal2.res2, y[2][var])
+
+    def test_sub_from_csv(self):
+
+        y = read.read_csv("../src/CSV FILES/Unit Test Subtraction.csv")
+
+        for var in range(len(y)):
+            self.cal2.subtraction(y[0][var], y[1][var])
+            self.assertEqual(self.cal2.res2, y[2][var]*-1)
+
+    def test_mul_from_csv(self):
+
+        y = read.read_csv("../src/CSV FILES/Unit Test Multiplication.csv")
+
+        for var in range(len(y)):
+            self.cal2.multiply(y[0][var], y[1][var])
+            self.assertEqual(self.cal2.res2, y[2][var])
+
+    def test_div_from_csv(self):
+
+        y = read.read_csv("../src/CSV FILES/Unit Test Division.csv")
+
+        for var in range(len(y)):
+            self.cal2.division(y[1][var], y[0][var])
+            self.assertEqual(int(self.cal2.res2), int(y[2][var]))
+
+    def test_square_from_csv(self):
+
+        y = read.read_csv("../src/CSV FILES/Unit Test Square.csv")
+
+        for var in range(len(y)):
+            self.cal2.square_(y[0][var])
+            self.assertEqual(int(self.cal2.res2), int(y[1][var]))
+
+    def test_sqrt_from_csv(self):
+
+        y = read.read_csv("../src/CSV FILES/Unit Test Square Root.csv")
+
+        for var in range(len(y)):
+            self.cal2.sqrt_(y[0][var])
+            self.assertEqual(int(self.cal2.res2), int(y[1][var]))
+
+    def test_array(self):
+        y = read.read_array("../src/CSV FILES/array.csv")
+        print(y[0])
+
+
+        self.assertEqual(0,0)
+
 
 
 
