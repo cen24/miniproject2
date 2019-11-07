0ed0a142801e8cf3bc6127440c00274b32b2ba0a
rutvik2611 <rutvik2611@gmail.com>
2019-11-07 16:08:46 -0500

    Feature:Exeption for mode function

--- Statisticss/computation/mode.py
+++ Statisticss/computation/mode.py
@@ -1,5 +1,9 @@
 import statistics
 
 def mode(list):
-    c = statistics.mode(list)
+    try:
+        c = statistics.mode(list)
+    except statistics.StatisticsError as e:
+         print("There was an error with the statistics module")
+        c = N.A
     return c
