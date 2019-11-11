66b251201b18b56272aaef5d748f1dd40cea205f
rutvik2611 <rutvik2611@gmail.com>
2019-11-11 00:42:59 -0500

    Feature:cintreval function and result saved

--- Statisticss/computation/extended/cinterval.py
+++ Statisticss/computation/extended/cinterval.py
@@ -1,38 +1,18 @@
-import math
-
-def cinterval(s):
-    # H, find the T-value (<30 sample size)
-    # (Calculate your T-Value by taking the difference between the mean and population mean
-    # (sample mean - pop mean)
-    # and dividing it over the standard deviation divided by the degrees (stdev - n)
-    # of freedom square root.
-
-    n = 10
-    stdev = 25
-    mean = 240
-    #b = (100 - a) / 100
-    #d = b / 2
-    #g = count(list)
-
-    df = n - 1
-    #1, above should be used  to calculate Tvalue
-    #https://www.statisticshowto.datasciencecentral.com/
-    # probability-and-statistics/confidence-interval/
-
-    tval = 2.262
-    h = (s - mean ) / (stdev / math.sqrt(n))
-    #h should equal Tval and tval should be removed
-    se = stdev / math.sqrt(n)
-    #2, above was used to calculate Standard of error
-    e = tval * se
-
-    # upper limit
-    c = mean + e
-
-    # lower limit
-    f = mean - e
-    return c, f, h
+from Statisticss.computation.populationmean import mean
+from Statisticss.computation.populationstdev import stdev
+from Calculator.computation.squareroot import sqrt
 
 
-#print (cinterval(95))
+def cintreval(list):
+    mean_var = mean(list)
+    cin = 0.95
+    value_of_z = (1-cin) / 2
+    stdev_var = stdev(list)
+    n = sqrt(len(list))
+    result = [mean_var - value_of_z*stdev_var / n, mean_var + value_of_z*stdev_var / n]
+    print(result)
+    return result
+
+
+
 
