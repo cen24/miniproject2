87db0e7c2b88a639d4d9eb6d2e293efaf09279db
rutvik2611 <rutvik2611@gmail.com>
2019-11-11 01:59:06 -0500

    Feature:Exception Handling Divide by Zero in Division

--- Calculator/computation/division.py
+++ Calculator/computation/division.py
@@ -1,5 +1,10 @@
 def div(a, b):
     a = float(a)
     b = float(b)
-    c = b / a
-    return c
+    try:
+        c = b / a
+        return  c
+    except ZeroDivisionError:
+        print("Number Cannot be Devided by zero")
+        c = 0
+        return c
