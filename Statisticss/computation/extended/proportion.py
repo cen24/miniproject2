a8a26a2ab3ae3fb093a1696eb34e075b936e6177
rutvik2611 <rutvik2611@gmail.com>
2019-11-10 22:43:30 -0500

    Fix:Proportion

--- Statisticss/computation/extended/proportion.py
+++ Statisticss/computation/extended/proportion.py
@@ -1,6 +1,16 @@
-def proportion(a, b):
-    c = a / b
-    return c
+# def proportion(a, b):
+#     c = a / b
+#     return c
+
+def proportion(population_list):
+    result = 0
+    num = 0
+    for i in population_list:
+        if (i % 2) == 0:
+            num = num + 1
+        #result = division(num, len(population_list))
+        result = num / len(population_list)
+    return result
 
 
 
